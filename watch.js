98c45bcb84aee233a6ffdf03c5ce9c9ea9b24f46
Colhon <colhon@media-saturn.com>
2018-04-11 12:22:59 +0300

    ttt

--- watch.js
+++ watch.js
@@ -1,7 +1,9 @@
 var watch = require('node-watch');
-const git = require('simple-git');
+const git = require('simple-git')();
  
 watch('test.txt', { recursive: true }, function(evt, name) {
   console.log('%s changed.', name);
-  git().commit('commit message', function(){console.log('commited')});
+  //git().commit('commit message', function(){console.log('commited')});
+  
+  require('simple-git')().commit('committed as "Some One"', 'test.txt');
 });
